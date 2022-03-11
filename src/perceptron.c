cabe06601fe277795e3c303f88129264b8bfe627
Fish-game-dev <andrewkawalec164@gmail.com>
2022-03-11 00:43:36 -0500

    Yes

--- src/perceptron.c
+++ src/perceptron.c
@@ -41,59 +41,125 @@ float map(int x, int in_min, int in_max, float out_min, float out_max)
 int main(int argc, char *argv[])
 {
 
-    if (argc < 3)
+    if (argc < 2)
     {
         printf("error not enuf files\n");
         exit(1);
     }
 
-    int width, height, channels;
-    unsigned char *img = stbi_load(argv[1], &width, &height, &channels, 0);
-
-    if(img == NULL)
+    if (strcmp(argv[1], "train") == 0)
     {
-        printf("error in loading the image\n");
-        exit(1);
+        int width, height, channels;
+        char buf[18];
+
+        for (unsigned int i = 1; i < atoi(argv[2]) + 1; i++)
+        {
+            printf("train %d", i);
+
+            int width2, height2, channels2;
+            unsigned char *w = stbi_load("w.jpg", &width2, &height2, &channels2, 0);
+
+            if (w == NULL) {
+                printf("error in loading the weights\n");
+            }
+
+            snprintf(buf, 18, "4/%d.jpg", i);
+            unsigned char *img = stbi_load(buf, &width, &height, &channels, 0);
+
+            if(img == NULL)
+            {
+                printf("error in loading the image\n");
+                exit(1);
+            }
+
+            unsigned char *img2 = GrayScale(img, width, height, channels);
+
+            unsigned char *w2 = GrayScale(w, width2, height2, channels2);
+
+            float ans = 0.0;
+
+            for (unsigned int i = 0; i < 30 * 30; i++)
+            {
+                ans = ans + (((float)img2[i] / 255.0) * ((float)w2[i] / 255.0));
+            }    
+
+            printf(" %f", ans);
+
+            if (ans < 70.0)
+            {
+                for (unsigned int i = 0; i < 30 * 30; i++)
+                {
+                    w2[i] =  map(img2[i] - 200 + w2[i], 0, 510, 0, 255);
+                }
+                stbi_write_jpg("w.jpg", width2, height2, 1, w2, 100);
+            }
+            else if (ans > 90.0)
+            {
+                for (unsigned int i = 0; i < 30 * 30; i++)
+                {
+                    w2[i] =  map(img2[i] + w2[i], 0, 510, 0, 255);
+                }
+                stbi_write_jpg("w.jpg", width2, height2, 1, w2, 100);
+            }
+
+            printf(" Done\n");
+
+        }
     }
-
-    int width2, height2, channels2;
-    unsigned char *w = stbi_load(argv[2], &width2, &height2, &channels2, 0);
-
-    if (w == NULL) {
-        printf("error in loading the weights\n");
-    }
-
-    unsigned char *img2 = GrayScale(img, width, height, channels);
-
-    unsigned char *w2 = GrayScale(w, width2, height2, channels2);
-
-    unsigned int ans;
-
-    for (unsigned int i = 0; i < 10 * 10; i++)
+    if (strcmp(argv[1], "clear") == 0)
     {
-        ans = ans + (img2[i] / 255) * (w2[i] / 255);
-    }
+        int width2, height2, channels2;
+        unsigned char *w = stbi_load("w.jpg", &width2, &height2, &channels2, 0);
 
-    if (ans > 3000000)
-    {
-        printf("Square?\n");
-        printf("was i right? ");
+        if (w == NULL) {
+            printf("error in loading the weights\n");
+        }
+
+        unsigned char *w2 = GrayScale(w, width2, height2, channels2);
+
+        for (unsigned int i = 0; i < 30 * 30; i++)
+        {
+            w2[i] = 0;
+        }
+        stbi_write_jpg("w.jpg", width2, height2, 1, w2, 100);
     }
     else
     {
-        printf("Not square?\n");
-        printf("was i right? ");
-    }
-    char t;
-    scanf(" %c", &t);
-    
-    if (t == 'n')
-    {
-        for (unsigned int i = 0; i < 10 * 10; i++)
+        int width, height, channels;
+        unsigned char *img = stbi_load(argv[1], &width, &height, &channels, 0);
+
+        if(img == NULL)
         {
-            w2[i] =  map(img2[i] + w2[i], 0, 510, 0, 255);
+            printf("error in loading the image\n");
+            exit(1);
         }
-        stbi_write_jpg("w.jpg", width2, height2, 1, w2, 100);
+
+        int width2, height2, channels2;
+        unsigned char *w = stbi_load("w.jpg", &width2, &height2, &channels2, 0);
+
+        if (w == NULL) {
+            printf("error in loading the weights\n");
+        }
+
+        unsigned char *img2 = GrayScale(img, width, height, channels);
+
+        unsigned char *w2 = GrayScale(w, width2, height2, channels2);
+
+        float ans = 0.0;
+
+        for (unsigned int i = 0; i < 30 * 30; i++)
+        {
+            ans = ans + (((float)img2[i] / 255.0) * ((float)w2[i] / 255));
+        }        
+
+        if (ans > 70.0 && ans < 90.0)
+        {
+            printf("Its a 4 %f\n", ans);
+        }
+        else
+        {
+            printf("Not a 4 %f\n", ans);
+        }
     }
     
     return 0;
